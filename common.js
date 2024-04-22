1a4b8f36199b2840d50b9a2fd45759b2b821071d
ksoy1201 <122020078+ksoy1201@users.noreply.github.com>
2024-04-22 11:42:50 +0900

    서버연결

---
+++ common.js
@@ -0,0 +1,36 @@
+const lodeData = async () => {
+  const response = await fetch(
+    'http://api.kcisa.kr/openapi/API_CNV_061/request?serviceKey=6a14b3d4-21f2-4b07-b66c-1a1bd3197ba3&numOfRows=10&pageNo=1',
+    {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    }
+  );
+  const data = await response.json();
+  const dataList = data.response.body.items.item;
+
+  renderTrip(dataList);
+};
+
+const renderTrip = (dataList) => {
+  console.log(dataList);
+  const tripList = dataList
+    .map(
+      (item) => `<li>
+         <div>
+           <span>${item.collectionDb}</span>
+           <p>${item.title}</p>
+           <p>${item.description}</p>
+           <span>${item.title}</span>
+           <span>${item.spatialCoverage}</span>
+          <a href="${item.url}"></a>
+         </div>
+      </li>`
+    )
+    .join('');
+  document.querySelector('.listCon').innerHTML = tripList;
+};
+lodeData();
