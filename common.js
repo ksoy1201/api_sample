4df5719b7eaf011dd4d43491a15fbb8879729e11
ksoy1201 <122020078+ksoy1201@users.noreply.github.com>
2024-04-25 17:50:59 +0900

    페이지네이션 수정중

--- common.js
+++ common.js
@@ -1,17 +1,3 @@
-const nav = document.querySelector('header > nav');
-
-// 검색기능
-const searchBtn = document.querySelector('.searchBtn');
-searchBtn.addEventListener('click', async () => {
-  const searchInput = document.querySelector('.inputText input');
-  const searchValue = searchInput.value;
-  console.log(searchValue);
-  const url = new URL(
-    `http://api.kcisa.kr/openapi/API_CNV_061/request?serviceKey=6a14b3d4-21f2-4b07-b66c-1a1bd3197ba3&numOfRows=10&q=${searchValue}&pageNo=1`
-  );
-  fetchDataAndRender(url);
-});
-
 // const getNewList = async (category) => {
 //   const url = new URL(
 //     `http://api.kcisa.kr/openapi/API_CNV_061/request?serviceKey=6a14b3d4-21f2-4b07-b66c-1a1bd3197ba3&numOfRows=10&category=${category}&pageNo=1`
@@ -25,31 +11,7 @@ searchBtn.addEventListener('click', async () => {
 //   let category = e.target.dataset.cate;
 //   getNewList(category);
 // });
-
-const fetchDataAndRender = async (url) => {
-  //gpt 도움 받아서 정리..
-
-  try {
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-    });
-
-    if (!response.ok) {
-      throw new Error('Failed to fetch data');
-    }
-
-    const data = await response.json();
-    const dataList = data.response.body.items.item;
-
-    renderTrip(dataList);
-  } catch (error) {
-    console.error('Error fetching data:', error);
-  }
-};
+const dataBord = document.querySelector('.listCon');
 
 const createHtml = (item) => {
   const imageUrl = item.image ? item.image : './img/none.png';
@@ -64,18 +26,87 @@ const createHtml = (item) => {
  </li>`;
 };
 
-const renderTrip = (dataList) => {
-  console.log(dataList);
-  const tripList = dataList.map((item) => createHtml(item)).join('');
-  document.querySelector('.listCon').innerHTML = tripList;
-};
+const inputText = document.querySelector('.inputText>input');
 
-const lodeData = async () => {
-  const url = new URL(
-    'http://api.kcisa.kr/openapi/API_CNV_061/request?serviceKey=6a14b3d4-21f2-4b07-b66c-1a1bd3197ba3&numOfRows=10&pageNo=1'
+// 나중에 검색 기능이나 페이지네이션 기능 추가 하기 위해서는 api 요청 parameter 정보를 변수로 빼서 사용하는 것이 좋아요
+const API_KEY = '6a14b3d4-21f2-4b07-b66c-1a1bd3197ba3';
+let numOfRows = 10;
+let pageNo = 1;
+let keyword = '';
+
+let totalCount = 0;
+let pageSize = 2;
+let page = 1;
+let groupSize = 10;
+
+const pagination = () => {
+  let pageGroup = Math.ceil(page / groupSize);
+  let lastPage = Math.min(
+    Math.ceil(totalCount / pageSize),
+    pageGroup * groupSize
   );
+  let firstPage = (pageGroup - 1) * groupSize + 1;
 
-  fetchDataAndRender(url);
+  let paginationhtml = `<button class="prev">이전</button>`;
+  for (let i = firstPage; i <= lastPage; i++) {
+    paginationhtml += `<button>${i}</button>`;
+  }
+  paginationhtml += `<button class="next">다음</button>`;
+  document.querySelector('.pgcon').innerHTML = paginationhtml;
 };
 
-lodeData();
+const moveToPage = async (pageNum, category) => {
+  console.log('moveToPage', category);
+  page = pageNum;
+  lodeData(numOfRows, pageNo, keyword, pageSize);
+};
+
+// 검색어를 입력하고 엔터를 누르면 검색어를 가져와서 api 요청을 보내는 함수
+inputText.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    keyword = e.target.value;
+    lodeData(numOfRows, pageNo, keyword, pageSize);
+  }
+});
+
+const fetchData = async (url) => {
+  console.log('url 확인', url.href);
+  try {
+    const res = await fetch(url, {
+      method: 'GET',
+      headers: {
+        accept: 'accept: application/json',
+      },
+    });
+    const data = await res.json();
+    totalCount = data.response.body.totalCount;
+    // console.log('전체 데이터 구조 확인', data);
+    console.log('전체 데이터 구조 확인', totalCount);
+    renderTrip(data.response.body.items.item);
+    pagination();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//동적으로 prameter를 변경해서 api 요청을 보내는 함수
+const lodeData = async (numOfRows, pageNo, keyword, pageSize) => {
+  const url = new URL(`http://api.kcisa.kr/openapi/API_CNV_061/request`);
+  url.searchParams.append('serviceKey', API_KEY);
+  url.searchParams.append('numOfRows', numOfRows);
+  url.searchParams.append('pageNo', pageNo);
+  url.searchParams.append('keyword', keyword);
+  url.searchParams.append('pageSize', pageSize);
+
+  fetchData(url);
+};
+
+const renderTrip = (dataList) => {
+  const startIndex = (page - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+  const paginatedData = dataList.slice(startIndex, endIndex);
+  const dataHtml = dataList.map((item) => createHtml(item)).join('');
+  dataBord.innerHTML = dataHtml;
+};
+
+lodeData(numOfRows, pageNo, keyword, pageSize); // 초기 셋팅값으로 api 요청
